1fc738f5bafcfd4844f22f4c5fc75e44b9378dce
Raulro03 <raulromeraortiz@gmail.com>
2024-12-19 17:25:42 +0100

    Ejercicio 7 y mejorar las rutas

--- app/Http/Controllers/Api/V3/ProductController.php
+++ app/Http/Controllers/Api/V3/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V3;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 
@@ -13,5 +14,47 @@ class ProductController extends Controller
         $products = Product::all()->paginate(9);
 
         return ProductResource::collection($products);
+    }
+    public function show(Product $product)
+    {
+        //abort_if(! auth()->user()->tokenCan('categories-show'), 403);
+
+        return new ProductResource($product);
+    }
+
+    /**
+     * Store a new product
+     *
+     * Creating a new product
+     *
+     * @bodyParam name string required The name of the product. Example: Electronics
+     */
+    public function store(StoreProductRequest $request)
+    {
+
+        abort_if(! auth()->user()->tokenCan('products-store'), 403);
+        $data = $request->all();
+
+        $product = Product::create($data);
+
+        return new ProductResource($product);
+    }
+
+    public function update(Product $product, StoreProductRequest $request)
+    {
+        abort_if(! auth()->user()->tokenCan('products-update'), 403);
+        $product->update($request->all());
+
+        return new ProductResource($product);
+    }
+
+    public function destroy(Product $product)
+    {
+        abort_if(! auth()->user()->tokenCan('products-destroy'), 403);
+        $product->subcategories()->detach();
+        $product->delete();
+
+        //return response(null, Response::HTTP_NO_CONTENT);
+        return response()->noContent();
     }
 }
---
+++ app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => ['required'],
+            'description' => ['required'],
+            'price' => ['required', 'integer'],
+
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
